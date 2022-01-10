4967b5d77495a4f01fa5a0bb68bb2bc869d59e9a
Hector <atiliogoulart@gmail.com>
2022-01-10 13:32:58 -0300

    Updates

--- app/client/models.py
+++ app/client/models.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 
 class ClientModel(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField("Name", max_length=55)
-    email = models.EmailField("Email", max_length=100)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=30, default="")
 
     def __str__(self):
         return self.name
